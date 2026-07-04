42463fed9668ca61a09ea8cb04321b3fd7481e44
Nandan Raichura <nandanraichura4@gmail.com>
2026-07-04 14:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (40.62%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void helper(int idx,vector<int> &nums,vector<vector<int>> &ans,vector<int> ds){
+        ans.push_back(ds);
+
+        for(int i=idx;i<nums.size();i++){
+            if(i!=idx && nums[i]==nums[i-1]){
+                continue;
+            }// avoiding the duplicates only this thing  is changes from the subsets probelm
+
+            ds.push_back(nums[i]);
+            // calling for next
+            
+            helper(i+1,nums,ans,ds);
+
+            ds.pop_back();//while backtrack
+            
+
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        // sortinng is necessary becz if we did it all the duplicates cames togethere
+        sort(nums.begin(),nums.end());
+        helper(0,nums,ans,ds);
+
+        return ans;
+    }
+};
