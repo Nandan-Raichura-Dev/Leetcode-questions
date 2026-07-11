534722acff3dec8e3422f15f6aa55da61b39aa33
Nandan Raichura <nandanraichura4@gmail.com>
2026-07-11 19:13:28 +0530

    Time: 67 ms (23.23%) | Memory: 150.9 MB (65.76%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,30 +1,56 @@
-class MinStack {// brute force will be to store the elements in the stack with pair, so we wi;; store the minimum also with every element 
+class MinStack {
 public:
-    stack<pair<int,int>> s;
-
+    stack<long long> s;
+    long long min=INT_MAX;
     MinStack() {
         
     }
     
     void push(int value) {
         if(s.empty()){
-            s.push({value,value});
-        }
-        else{
-            s.push({value,min(value,s.top().second)});
+            min=value;
+            s.push(value);
+        }else{// we will not store the actual value of the element in the stack becz if we do that, and change the minium , if the pop opreation have been done , then we have told to take out the new min , we will not able to get back to the old min, we will have the minium, of the element we have poped
+
+            if(value>min){
+                s.push(value);// min is not needed to change so we push the element without any changes
+            }else{
+                s.push(2LL*value-min);
+
+                min=value;
+            }
         }
     }
     
     void pop() {
+        if(s.empty()){
+            return;
+        }
+        long long n=s.top();
+        if(n<min){// if the n is modeified value, it will be lesser then the min;tjen we have to back to the prev min
+
+        min=2*min-n;
+
+        }
         s.pop();
     }
     
     int top() {
-        return s.top().first;
+        if(s.empty()){
+            return -1;;
+        }
+
+        long long n=s.top();
+        if(min<n){// in this case the n is not modified value, so we have to return the top
+            return n;
+        }else{
+            return min;// in this case min will be our top , as well as the min value of the entire stack
+
+        }
     }
     
     int getMin() {
-        return s.top().second;
+       return min;
     }
 };
 
