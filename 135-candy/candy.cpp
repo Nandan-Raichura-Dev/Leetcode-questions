ebb842e23389f67f4924d04960bc4b8dac240074
Nandan Raichura <nandanraichura4@gmail.com>
2026-07-31 21:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (92.01%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,41 +1,76 @@
 class Solution {// we will cal for lrft and then right, and the connsider max of  it, 
-public:
+public:// optimal: through the slope method
     int candy(vector<int>& ratings) {
-         
+         int i=1;
          int n=ratings.size();
-         vector<int> left(n);
-         left[0]=1;// there is no left element 
-         vector<int> right(n);
-         right[n-1]=1;// there iss no right elemeent
 
-         // for left
-         for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
-            }else{
-                left[i]=1;
+         int sum=1;
+         int peak=1;
+
+         while(i<n){
+            if(ratings[i]==ratings[i-1]){// slope is straight
+                sum+=1;
+                i++;
+                continue;
+
             }
-         }
 
-         // for right
-
-         for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                 right[i]=right[i+1]+1;
-            }else{
-               right[i]=1;
+            while(i<n && ratings[i]>ratings[i-1]){// peak increases
+                peak+=1;
+                sum+=peak;
+                i++;
             }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){// dec slope
+                sum+=down;
+                i++;
+                down++;
+                
+            }
+
+            if(down>peak){
+                sum+=down-peak;
+            }
+
+            peak=1;
          }
-
-         // taling ,ax from bpth becz we want the elememnt satisfies bith side conditions
-
-         int sum=0;
-
-         for(int i=0;i<n;i++){
-            sum=sum+max(left[i],right[i]);
-         }
-
-        return sum;
-
+         return sum;
     }
 };
+
+
+
+// int n=ratings.size();
+//          vector<int> left(n);
+//          left[0]=1;// there is no left element 
+//          vector<int> right(n);
+//          right[n-1]=1;// there iss no right elemeent
+
+//          // for left
+//          for(int i=1;i<n;i++){
+//             if(ratings[i]>ratings[i-1]){
+//                 left[i]=left[i-1]+1;
+//             }else{
+//                 left[i]=1;
+//             }
+//          }
+
+//          // for right
+
+//          for(int i=n-2;i>=0;i--){
+//             if(ratings[i]>ratings[i+1]){
+//                  right[i]=right[i+1]+1;
+//             }else{
+//                right[i]=1;
+//             }
+//          }
+
+//          // taling ,ax from bpth becz we want the elememnt satisfies bith side conditions
+
+//          int sum=0;
+
+//          for(int i=0;i<n;i++){
+//             sum=sum+max(left[i],right[i]);
+//          }
+
+//         return sum;
